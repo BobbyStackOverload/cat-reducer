8a7c1401889a29a29a72efe30a9d2d9288a43b47
Bobby W Bryant <sirbob1999@gmail.com>
2020-04-07 10:29:42 -0400

    added buttons for play and nap

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { eat, play, nap } from '../redux/actions';
 
-const Activity = props => (
-    <h1>Activity Component</h1>
+const Activity = ({ activity, eat, play, nap }) => (
+    <>
+    <h1>What is the cat doing?</h1>
+    <p>The cat is {activity}</p>
+    <button onClick={()=> eat()}>eating</button>
+    <button onClick={()=> play()}>playing</button>
+    <button onClick={()=> nap()}>napping</button>
+    </>
 )
+const mapStateToProps = state => {
+    const { activity } = state;
+    return activity;
+}
 
-export default Activity;
+export default connect(
+    mapStateToProps, //mapStateToProps what do I want?
+    { eat, play, nap } //mapDispatchToProps What do I want to do?
+    )(Activity);
